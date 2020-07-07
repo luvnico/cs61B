3ad50d7415cdee51e99acab171720e5d8e239354
luvnico <ys2k6@mail.missouri.edu>
2020-07-07 21:30:46 +0800

    Finished Drawing the Initial Universe State (main)

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -5,6 +5,9 @@ import java.io.FileInputStream;
  * The goal of this class is to simulate a universe specified in one of the data files.
  */
 public class NBody {
+    private static final String DATA_DIR = "data/";
+    private static final String IMG_DIR = "images/";
+
     public static double readRadius(String filename) {
         In in = new In(filename);
         in.readLine(); // skip line 1
@@ -31,5 +34,22 @@ public class NBody {
         }
 
         return bodies;
+    }
+
+    public static void main(String[] args) {
+        double T = Double.valueOf(args[0]);
+        double dt = Double.valueOf(args[1]);
+        String filename = DATA_DIR + args[2];
+        double universeRadius = readRadius(filename);
+        // initializating canvas
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setScale(-universeRadius, universeRadius);
+        StdDraw.clear();
+        StdDraw.picture(0, 0, IMG_DIR + "starfield.jpg");
+        Body[] bodies = readBodies(filename);
+        for (Body body : bodies) {
+            StdDraw.picture(body.xxPos, body.yyPos, IMG_DIR + body.imgFileName);
+        }
+        StdDraw.show();
     }
 }
